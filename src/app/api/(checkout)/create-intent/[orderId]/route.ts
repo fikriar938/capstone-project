8ed2d2cc708f072ed508dbbaa9caac3deee4510f
fikriar938 <fikriarifin938@gmail.com>
2023-12-25 19:15:55 +0700

    deploy2

--- src/app/api/(checkout)/create-intent/[orderId]/route.ts
+++ src/app/api/(checkout)/create-intent/[orderId]/route.ts
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(
-  request: NextRequest,
   { params }: { params: { orderId: string } }
 ) {
   const { orderId } = params;
