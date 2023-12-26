f2ab0393bfc205add5d729a3ab94935929dc6171
fikriar938 <fikriarifin938@gmail.com>
2023-12-26 08:35:00 +0700

    deploy1d

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -21,7 +21,7 @@ const CartPage = () => {
       router.push("/login")
     }else{
       try {
-        const res = await fetch("http://localhost:3000/api/orders", {
+        const res = await fetch( process.env.URL + "/api/orders", {
           method: "POST",
           headers: {"Content-Type":"application/json"},
           body: JSON.stringify({
