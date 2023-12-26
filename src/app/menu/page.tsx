1851b0a7cbfbb5b4c08e66748a36ce4560f9d006
fikriar938 <fikriarifin938@gmail.com>
2023-12-26 08:31:29 +0700

    deploy1c

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React from "react";
 
 const getData = async ()=>{
-  const res = await fetch("http://localhost:3000/api/categories",{
+  const res = await fetch(process.env.URL + "/api/categories",{
     cache:"no-store"
   })
 
