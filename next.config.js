77bae4ee77a71abee1e0561ef4bb52655b224044
fikriar938 <fikriarifin938@gmail.com>
2023-12-28 07:33:52 +0700

    deploy3j

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 import dns from 'node:dns';
-const nextConfig = {   }
-dns.setDefaultResultOrder('ipv4first');
+const nextConfig = () => {  dns.setDefaultResultOrder('ipv4first'); }
+
 
 module.exports = nextConfig
