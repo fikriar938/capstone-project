e8abd9361cef04a4ce13a3c221cf16fd46baa08e
fikriar938 <fikriarifin938@gmail.com>
2023-12-30 12:49:05 +0700

    final2

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import CountDown from "./CountDown";
+import Countdown from "react-countdown/dist/LegacyCountdown";
 
 const Offer = () => {
   return (
@@ -12,7 +13,7 @@ const Offer = () => {
           Progressively simplify effective e-toilers and process-centric methods
           of empowerment. Quickly pontificate parallel.
         </p>
-        <CountDown/>
+        <Countdown />
         <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
       </div>
       {/* IMAGE CONTAINER */}
